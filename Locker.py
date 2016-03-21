17ea0a74c354e526f686dcbc5338a10e4403b6b3
hkizilhan <hkizilhan@gmail.com>
2016-03-21 23:25:25 +0200

    Shutdown delay button added.

--- Locker.py
+++ Locker.py
@@ -3,6 +3,10 @@ from winreg import *
 import tkinter as tk
 
 DEBUG=True
+SHUTDOWN_SECONDS_LIMIT = 30
+DELAYED_SHUTDOWN_SECONDS_LIMIT = 900
+CHECK_SECONDS = 5000 # ms
+DELAY_BUTTON_COUNTER_START = 5
 
 # No cable
 #   Medya Durumu  . . . . . . . . . . : Medya Bağlantısı kesildi
@@ -61,14 +65,17 @@ class Locker_Window():
         self.root.wm_attributes("-topmost", 1) # Always on top
         self.root.geometry("800x600")
         self.root.config(bg="red")
-
-        self.check_seconds = 5000 # check every 5 secons
-        self.shutdown_seconds = 0
-        self.shutdown_seconds_limit = 30
+        self.delay_button = tk.Button(self.root, text="", command=self.delay_button_click)
         
-        self.state = False
-        self.visible = True
-
+        self.check_seconds = CHECK_SECONDS
+        self.shutdown_seconds = 0
+        self.shutdown_seconds_limit = SHUTDOWN_SECONDS_LIMIT
+        
+        self.usb_key_was_plugged=False
+        self.delay_button_timer_id=None
+        self.delay_button_timer_started=False
+        self.delay_button_counter = DELAY_BUTTON_COUNTER_START
+        
         if DEBUG:
             # Testing
             self.root.bind("<F11>", lambda state:self.set_fullscreen(state=True))
@@ -93,8 +100,15 @@ class Locker_Window():
             self.unlock()
             self.shutdown_seconds = 0
         else:
-            print("LOCKED")
-            self.lock()
+            if not key_present:
+                # Reason = No USB Key
+                print("LOCKED - No Usb Present")
+                self.lock(reason="no_usb")
+            else:
+                # Reason = No cable Connected
+                print("LOCKED - No Cable Connected")
+                self.lock(reason="no_cable")
+            
             self.shutdown_seconds += (self.check_seconds / 1000)
             if self.shutdown_seconds > self.shutdown_seconds_limit:
                 # shutdown limit passed, shutdown computer
@@ -103,11 +117,19 @@ class Locker_Window():
         self.main_timer_id = self.root.after(self.check_seconds, self.main_timer)
         # self.root.after_cancel(after_id)
 
-    def lock(self):
+    def lock(self, reason=None):
         self.root.deiconify()
+        if reason=="no_usb":
+            if self.usb_key_was_plugged:
+                if not self.delay_button_timer_started:
+                    self.delay_button_timer_started = True
+                    self.delay_button_timer()
+        
 
     def unlock(self):
         self.root.withdraw()
+        self.usb_key_was_plugged = True
+        self.shutdown_seconds_limit = SHUTDOWN_SECONDS_LIMIT
 
     def check_usb(self):
         k_name, k_data = get_usb_key()
@@ -121,6 +143,39 @@ class Locker_Window():
         else:
             return False
             
+    def delay_button_click(self):
+        # Set shutdown 15 minutes later
+        self.shutdown_seconds_limit = DELAYED_SHUTDOWN_SECONDS_LIMIT
+        
+        # Clear counters
+        self.delay_button_counter = DELAY_BUTTON_COUNTER_START
+        self.delay_button_timer_started = False
+        self.delay_button.place_forget()
+        self.usb_key_was_plugged=False
+        # Disable timer
+        self.delay_button.after_cancel(self.delay_button_timer_id)
+        
+        
+    def delay_button_timer(self):
+        
+        if self.delay_button_counter == DELAY_BUTTON_COUNTER_START:
+            # First Run
+            self.delay_button.place(x=20, rely=.5, height=100, width=150)
+        
+        self.delay_button['text'] = "15 Dakika Beklet...({})".format(int(self.delay_button_counter))
+        
+        self.delay_button_counter -= 1
+        if self.delay_button_counter < 0:
+            # Passed 5 seconds, remove button, reset counters
+            self.delay_button_counter = DELAY_BUTTON_COUNTER_START
+            self.delay_button_timer_started = False
+            self.delay_button.place_forget()
+            self.usb_key_was_plugged=False
+            return
+            
+        # Set delay Button timer
+        self.delay_button_timer_id = self.delay_button.after(1000, self.delay_button_timer)
+    
     def close_request(self):
         pass
 
